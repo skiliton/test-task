fe44fb46ab81dbbcce491d925f469e1ab8963074
Ivan Repeta <ivan.repeta.work@gmail.com>
2021-07-12 17:14:30 +0300

    Add task 2 solution

--- src/main/java/com/repeta/App.java
+++ src/main/java/com/repeta/App.java
@@ -1,6 +1,7 @@
 package com.repeta;
 
 import static com.repeta.Task1.addTwoDigits;
+import static com.repeta.Task2.largestNumber;
 
 /**
  * Hello world!
@@ -10,9 +11,15 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("addTwoDigits invocations");
         System.out.println(addTwoDigits(10));
         System.out.println(addTwoDigits(29));
         System.out.println(addTwoDigits(48));
         System.out.println(addTwoDigits(99));
+
+        System.out.println("\nlargestNumber invocations");
+        System.out.println(largestNumber(1));
+        System.out.println(largestNumber(2));
+        System.out.println(largestNumber(9));
     }
 }
---
+++ src/main/java/com/repeta/Task2.java
@@ -0,0 +1,7 @@
+package com.repeta;
+
+public class Task2 {
+    static int largestNumber(int n){
+        return (int)Math.pow(10,n)-1;
+    }
+}
